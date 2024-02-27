4c55b100c3c06c802017c4c9f7d78742e4415950
ELCRND <jwhh2a@gmail.com>
2024-02-27 20:10:11 +0300

    modified:   css/styles.css modified:   css/styles.css.map renamed:    img/useCases/bg/use_cases_bg.png -> img/bg/use_cases_bg.png new file:   img/footer/email_icon.svg new file:   img/footer/facebook_icon.svg new file:   img/footer/instagram_icon.svg new file:   img/footer/location_icon.svg new file:   img/footer/phone_icon.svg new file:   img/footer/twitter_icon.svg modified:   index.html modified:   js/ourTestimonials.js modified:   scss/base/_base.scss modified:   scss/base/_variables.scss modified:   scss/block/FAQ.scss new file:   scss/block/footer.scss modified:   scss/block/header.scss modified:   scss/block/hero.scss modified:   scss/block/ourFeatures.scss modified:   scss/block/ourProducts.scss modified:   scss/block/ourTestimonials.scss new file:   scss/block/startYourBank.scss modified:   scss/block/useCases.scss modified:   scss/main.scss

--- js/ourTestimonials.js
+++ js/ourTestimonials.js
@@ -1,13 +1,14 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 const swiper = new Swiper(".swiper__testimonials", {
   slidesPerView: 3,
+  centeredSlides: true,
   allowTouchMove: false,
   loop: true,
-  centeredSlides: true,
-  //   centeredSlidesBounds: true,
+  free: true,
 
   autoplay: {
-    delay: 3000,
+    delay: 5000,
+    disableOnInteraction: true,
   },
 
   navigation: {
