b40ce44697926349527601380884c33a987c951d
ELCRND <jwhh2a@gmail.com>
2024-06-25 18:03:41 +0300

    modified:   index.html 	renamed:    js/ourTestimonials.js -> js/swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,27 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
+const swiperTransactions = new Swiper(".swiper-transactions", {
+  slidesPerView: "auto",
+  grabCursor: true,
+  direction: "vertical",
+  effect: "creative",
+  loop: true,
+  mousewheel: true,
+  creativeEffect: {
+    limitProgress: 2,
+    next: {
+      translate: [18, 65, 0],
+      opacity: 0.55,
+    },
+    prev: {
+      translate: [0, 0, 0],
+    },
+  },
+  autoplay: {
+    delay: 2000,
+  },
+});
+
 const swiperTestimonials = new Swiper(".swiper__testimonials", {
   slidesPerView: "auto",
   loop: true,
