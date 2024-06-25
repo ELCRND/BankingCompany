03dce700ce7b4de6c2cbf53ca9e05948b77e068f
ELCRND <jwhh2a@gmail.com>
2024-06-25 13:39:38 +0300

    modified:   css/styles.css 	modified:   css/styles.css.map 	deleted:    img/bg/use_cases_bg.png 	new file:   img/bg/use_cases_bg.webp 	deleted:    img/header/header_bg.png 	new file:   img/header/header_bg.webp 	deleted:    img/header/header_bg_mobile.png 	new file:   img/header/header_bg_mobile.webp 	deleted:    img/hero/flagOfIndia_icon.png 	new file:   img/hero/flagOfIndia_icon.webp 	deleted:    img/hero/flagOfUSA_icon.png 	new file:   img/hero/flagOfUSA_icon.webp 	deleted:    img/hero/hero_bg.png 	new file:   img/hero/hero_bg.webp 	deleted:    img/hero/hero_bg_laptop.png 	new file:   img/hero/hero_bg_laptop.webp 	deleted:    img/hero/hero_bg_mobile.png 	new file:   img/hero/hero_bg_mobile.webp 	deleted:    img/hero/prompt_logo.png 	new file:   img/hero/prompt_logo.webp 	modified:   index.html 	modified:   js/animation.js 	modified:   package.json 	modified:   scss/base/_base.scss 	modified:   scss/pages/general/header.scss 	modified:   scss/pages/general/startYourBank.scss 	modified:   scss/pages/home/hero.scss 	modified:   scss/pages/home/useCases.scss

--- js/animation.js
+++ js/animation.js
@@ -1,41 +1,41 @@
-window.innerWidth >= 1024 &&
-  (() => {
-    const productsCards = document.querySelector(".products");
-    const useCasesCardsIndividuals = document.querySelector(
-      ".individuals__options-list"
-    );
-    const useCasesCardsBusiness = document.querySelector(
-      ".business__options-list"
-    );
-    const featuresCards = document.querySelector(".details__cards");
+// window.innerWidth >= 1024 &&
+//   (() => {
+//     const productsCards = document.querySelector(".products");
+//     const useCasesCardsIndividuals = document.querySelector(
+//       ".individuals__options-list"
+//     );
+//     const useCasesCardsBusiness = document.querySelector(
+//       ".business__options-list"
+//     );
+//     const featuresCards = document.querySelector(".details__cards");
 
-    const intersectionObserver = new IntersectionObserver(
-      (entries) => {
-        const element = entries[0].target;
+//     const intersectionObserver = new IntersectionObserver(
+//       (entries) => {
+//         const element = entries[0].target;
 
-        if (entries[0].intersectionRatio <= 0) return;
-        else if (element.classList.contains("products")) {
-          productsCards.classList.add("appearanceFromBottom");
-          productsCards.classList.remove("opacity");
-        } else if (element.classList.contains("individuals__options-list")) {
-          element.classList.add("appearanceFromEdges");
-        } else if (element.classList.contains("business__options-list")) {
-          element.classList.add("appearanceFromEdges");
-        } else if (element.classList.contains("details__cards")) {
-          element.classList.add("increaseToRight");
-        }
-      },
-      {
-        threshold: 1,
-      }
-    );
+//         if (entries[0].intersectionRatio <= 0) return;
+//         else if (element.classList.contains("products")) {
+//           productsCards.classList.add("appearanceFromBottom");
+//           productsCards.classList.remove("opacity");
+//         } else if (element.classList.contains("individuals__options-list")) {
+//           element.classList.add("appearanceFromEdges");
+//         } else if (element.classList.contains("business__options-list")) {
+//           element.classList.add("appearanceFromEdges");
+//         } else if (element.classList.contains("details__cards")) {
+//           element.classList.add("increaseToRight");
+//         }
+//       },
+//       {
+//         threshold: 1,
+//       }
+//     );
 
-    productsCards.classList.add("opacity");
-    intersectionObserver.observe(productsCards);
+//     productsCards.classList.add("opacity");
+//     intersectionObserver.observe(productsCards);
 
-    intersectionObserver.observe(useCasesCardsIndividuals);
+//     intersectionObserver.observe(useCasesCardsIndividuals);
 
-    intersectionObserver.observe(useCasesCardsBusiness);
+//     intersectionObserver.observe(useCasesCardsBusiness);
 
-    intersectionObserver.observe(featuresCards);
-  })();
+//     intersectionObserver.observe(featuresCards);
+//   })();
